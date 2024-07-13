71f9238bae40e2b17618926e41ab4b624ee7381b
CharityNambi05 <charity.nambi@icloud.com>
2024-07-13 15:20:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+let employee = {
+    name: "Alice",
+    role: "Developer"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+ 
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+
+  let newEmployee = updateEmployeeWithKeyAndValue(employee, "address", "123 Main St");
+  console.log(newEmployee); 
+  console.log(employee); 
+  
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, "role", "Manager");
+  console.log(employee); 
+
+
